70e421d9333aefc2f9d31d0ff2055baa9431dd8a
Efim Dezhin <efim.dezhin@g42.ai>
2022-04-26 09:52:12 +0400

    change translation storage

--- template/src/components/TranslateProvider.tsx
+++ template/src/components/TranslateProvider.tsx
@@ -1,53 +1,54 @@
-import React, {useCallback, useEffect} from 'react';
-import {IntlProvider} from 'react-intl';
-import {log} from '@/utils';
-import {ConfigProvider} from 'antd';
-import {StyleSheetManager} from 'styled-components';
+import React, { useCallback, useEffect } from 'react';
+import { IntlProvider } from 'react-intl';
+import { log } from '@/utils';
+import { ConfigProvider } from 'antd';
+import { StyleSheetManager } from 'styled-components';
 import rtlcss from 'stylis-rtlcss';
-import enGB from 'antd/lib/locale/en_GB';
-import arEG from 'antd/lib/locale/ar_EG';
-import {Locale, useLocaleStore} from '@/store/locale';
-import {GlobalStyle} from './GlobalStyles';
+import { Locale, useLocaleStore } from '@/store/locale';
+import { GlobalStyle } from './GlobalStyles';
 
-const getAntLocale = (v: Locale) => {
-  switch (v) {
-    case Locale.AR:
-      return arEG;
-    default:
-    case Locale.EN:
-      return enGB;
-  }
+import enWords from '@/assets/translates/en.json';
+import enGB from 'antd/lib/locale/en_GB';
+
+import arWords from '@/assets/translates/ar.json';
+import arEG from 'antd/lib/locale/ar_EG';
+
+const vocabulary: Record<Locale, any> = {
+  [Locale.EN]: enWords,
+  [Locale.AR]: arWords
 };
+
+const antVocabulary = {
+  [Locale.EN]: enGB,
+  [Locale.AR]: arEG
+}
 
 const direction = (locale: Locale) => [Locale.AR].includes(locale) ? 'rtl' : 'ltr';
 
-export const TranslateProvider: React.FC = ({children}: React.PropsWithChildren<{}>) => {
+export const TranslateProvider: React.FC = ({ children }: React.PropsWithChildren<{}>) => {
   const {
-    locale,
-    messages
+    locale
   } = useLocaleStore();
 
   const errorHandler = useCallback((data) => {
-    const {code, descriptor: {id}} = data;
-    if (Object.keys(messages).length) {
-      if (code === 'MISSING_TRANSLATION') {
-        log(`Cannot find translate "${id}" in "${locale}"`);
-      } else {
-        log(data);
-      }
+    const { code, descriptor: { id } } = data;
+    if (code === 'MISSING_TRANSLATION') {
+      log(`Cannot find translate "${id}" in "${locale}"`);
+    } else {
+      log(data);
     }
-  }, [locale, messages]);
+  }, [locale]);
 
   useEffect(() => {
     document.documentElement.dir = direction(locale);
   }, [locale]);
 
   return (
-    <StyleSheetManager {...(direction(locale) === 'rtl' ? {stylisPlugins: [rtlcss]} : {})}>
+    <StyleSheetManager {...(direction(locale) === 'rtl' ? { stylisPlugins: [rtlcss] } : {})}>
       <>
-        <GlobalStyle/>
-        <ConfigProvider locale={getAntLocale(locale)} direction={direction(locale)}>
-          <IntlProvider locale={locale} messages={messages} onError={errorHandler}>
+        <GlobalStyle />
+        <ConfigProvider locale={antVocabulary[locale]} direction={direction(locale)}>
+          <IntlProvider locale={locale} messages={vocabulary[locale]} onError={errorHandler}>
             <>
               {children}
             </>
--- template/src/store/locale.ts
+++ template/src/store/locale.ts
@@ -1,6 +1,4 @@
 import create from 'zustand';
-import enWords from '@/assets/translates/en.json'
-import arWords from '@/assets/translates/ar.json'
 
 export enum Locale {
   EN = 'en',
@@ -8,28 +6,13 @@ export enum Locale {
 }
 
 const defaultLocale = Locale.EN;
-const defaultMessages = enWords;
 
 interface Store {
   locale: Locale;
-  messages: Record<string, any>;
   setLocale: (locale: Locale) => void;
 }
 
 export const useLocaleStore = create<Store>((set, get) => ({
   locale: defaultLocale,
-  messages: defaultMessages,
-  setLocale: (locale: Locale) => {
-    let messages = {};
-    switch (locale) {
-      case Locale.AR:
-        messages = arWords;
-        break
-      default:
-      case Locale.EN:
-        messages = enWords;
-        break;
-    }
-    set(state => ({locale, messages}))
-  }
+  setLocale: (locale: Locale) => set(state => ({locale}))
 }));
